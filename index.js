bb75f4edb51dac953b154c047a54408c0b4e7dbe
Selin Ayyildiz <xcadyix@gmail.com>
2022-06-13 20:42:16 -0400

    commit tree 8c505f02b143862e8392a49ad22cf47f5e307fa7

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(record) {
+  const superB = record.find(record => record.result === "W") 
+  if (record[""] === "N/A") {}
+   else { return superB.year}
+}
